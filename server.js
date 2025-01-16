b7f596ceafb4a9a0ade291c49b82482b70b544d0
Pavan9567 <pavank9567@gmail.com>
2025-01-16 15:03:32 +0530

    Backend Base

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET','POST','PUT','DELETE'],
+}));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB Atlas'))
